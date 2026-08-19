a9fc5bad66cdcc07348445bb60314ea65fc72c4f
Graham MacDonald <grahamamacdonald@gmail.com>
2026-08-19 17:17:29 +0100

    ci: install binutils and fallback to objcopy

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -318,7 +318,7 @@ fn objcopy() -> String {
         if path.exists() {
             path.into_os_string().into_string().unwrap()
         } else {
-            "llvm-objcopy".into()
+            "objcopy".into()
         }
     };
     env_or("OBJCOPY", &llvm_objcopy)
