4df0f921ea8001278a703bc2a79b8401335536a2
Graham MacDonald <grahamamacdonald@gmail.com>
2026-08-19 14:16:23 +0100

    fmt: fix formatting

--- aarch64/src/reg/cnt_el0.rs
+++ aarch64/src/reg/cnt_el0.rs
@@ -1,7 +1,9 @@
 use core::fmt;
 
 use aarch64_cpu::asm::barrier;
-use aarch64_cpu::registers::{CNTFRQ_EL0, CNTP_CTL_EL0, CNTP_CVAL_EL0, CNTPCT_EL0, Readable, Writeable};
+use aarch64_cpu::registers::{
+    CNTFRQ_EL0, CNTP_CTL_EL0, CNTP_CVAL_EL0, CNTPCT_EL0, Readable, Writeable,
+};
 use bitstruct::bitstruct;
 
 // CNTPCT_EL0 — 64-bit physical counter
